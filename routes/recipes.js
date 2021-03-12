602eeabcc3d317bb7ddd462041ae1bd0c58a4b5c
Sheila León Santana <sheilals.1993@gmail.com>
2021-03-12 23:56:47 +0000

    cambio de proyecto: crea endpoint /recipes muestra por pantalla todos elementos de la tabla de la bd en json

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var recipesController = require('../controllers/recipesController');
 
@@ -17,7 +18,25 @@ router.route('/:id')
 /*
 router.get('/', function(req, res) {
     res.json({ username: 'Flavio' });
-});
+});lo
 */
+/*
+module.exports = router;
+*/
+
+
+const express = require('express');
+const router = express.Router();
+const programmingLanguages = require('../services/recipes');
+
+/* GET programming languages. */
+router.get('/', async function(req, res, next) {
+  try {
+    res.json(await programmingLanguages.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting programming languages `, err.message);
+    next(err);
+  }
+});
 
 module.exports = router;
